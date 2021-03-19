c63310afe07d7d1479a2383cfd8189c7863f57e7
patrick <patrick@fedora01.linux.local>
2021-03-18 21:19:52 -0500

    Made UI changes to make application actually look decent and fix issues from original template taken from an old project

--- src/main/java/com/example/passwordlocker/controllers/accounts/CreateAccount.java
+++ src/main/java/com/example/passwordlocker/controllers/accounts/CreateAccount.java
@@ -1,4 +1,25 @@
 package com.example.passwordlocker.controllers.accounts;
 
+import com.example.passwordlocker.repositories.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value={"/accounts/create", "/accounts/create/"})
 public class CreateAccount {
+
+    @Autowired
+    private AccountRepository repository;
+
+    @GetMapping(value={"/", ""})
+    public String createIndex() {
+        return "create-account";
+    }
+
+    // @PostMapping(value={"/", ""})
+
 }
