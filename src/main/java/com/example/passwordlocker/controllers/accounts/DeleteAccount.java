8331b133ba90b7fab96d7729d8c541dd915d092e
patrick <patrick@fedora01.linux.local>
2021-03-26 15:06:53 -0500

    Added a disabled field for account IDs in create-account.html, create DeleteAccount controller to handle deleting accounts

--- src/main/java/com/example/passwordlocker/controllers/accounts/DeleteAccount.java
+++ src/main/java/com/example/passwordlocker/controllers/accounts/DeleteAccount.java
@@ -1,4 +1,24 @@
 package com.example.passwordlocker.controllers.accounts;
 
+import com.example.passwordlocker.models.Account;
+import com.example.passwordlocker.repositories.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class DeleteAccount {
+
+    @Autowired
+    private AccountRepository repository;
+
+    @GetMapping("/accounts/delete")
+    public String deleteAccount(@RequestParam(name="id", required = true) long id) {
+        Account account = repository.findById(id).orElse(new Account());
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+        }
+        return "redirect:/accounts";
+    }
 }
