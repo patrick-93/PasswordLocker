f5cfa585c5bc732aeb6767aca4867ace0bfe3e1b
patrick <patrick@fedora01.linux.local>
2021-03-27 15:00:34 -0500

    Added way to store passwords encrypted and decrypt them when viewing an account's details using the app.secret-key and app.secret-salt properties

--- src/main/java/com/example/passwordlocker/controllers/accounts/CreateAccount.java
+++ src/main/java/com/example/passwordlocker/controllers/accounts/CreateAccount.java
@@ -1,9 +1,11 @@
 package com.example.passwordlocker.controllers.accounts;
 
+import com.example.passwordlocker.config.PasswordConfig;
 import com.example.passwordlocker.models.Account;
 import com.example.passwordlocker.repositories.AccountRepository;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +19,12 @@ public class CreateAccount {
     @Autowired
     private AccountRepository repository;
 
+    @Value("${app.secret-key}")
+    private String key;
+
+    @Value("${app.secret-salt}")
+    private String salt;
+
     @GetMapping(value={"/", ""})
     public String createAccountGet(Model model) {
         model.addAttribute("account", new Account());
@@ -28,8 +36,12 @@ public class CreateAccount {
     public String createAccountPost(@ModelAttribute("account") Account account, BindingResult bindingResult) {
 
         if (bindingResult.hasErrors()) {
-            return "/";
+            return "accounts/create-account";
         }
+        String oldPassword = account.getPassword();
+        String newPassword = PasswordConfig.encryptString(key, salt, oldPassword);
+        account.setPassword(newPassword);
+
         repository.save(account);
         return "redirect:/accounts";
     }
--- src/main/java/com/example/passwordlocker/controllers/accounts/ReadAccount.java
+++ src/main/java/com/example/passwordlocker/controllers/accounts/ReadAccount.java
@@ -1,8 +1,11 @@
 package com.example.passwordlocker.controllers.accounts;
 
+import com.example.passwordlocker.config.PasswordConfig;
 import com.example.passwordlocker.models.Account;
 import com.example.passwordlocker.repositories.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +24,12 @@ public class ReadAccount {
     @Autowired
     private AccountRepository repository;
 
+    @Value("${app.secret-key}")
+    private String key;
+
+    @Value("${app.secret-salt}")
+    private String salt;
+
     @GetMapping(value="")
     public ModelAndView showAll() {
         ModelAndView mav = new ModelAndView("accounts/accounts");
@@ -29,11 +38,23 @@ public class ReadAccount {
     }
 
     @GetMapping("/details")
-    public String accountDetail(@RequestParam(name = "id", required = true) long id, Model model) {
+    public String accountDetail(@RequestParam(name = "id") long id, Model model) {
         Account account = repository.findById(id).orElse(new Account());
-        model.addAttribute("account", account);
-        return "accounts/account-details";
-    }
 
+        // Check if we have an actual account object, if not show an error page
+        if (account.getUsername() != null) {
+            // Decrypt the password so it displays
+            String encryptedPassword = account.getPassword();
+            String decryptedPassword = PasswordConfig.decryptString(key, salt, encryptedPassword);
+            account.setPassword(decryptedPassword);
+
+            model.addAttribute("account", account);
+            return "accounts/account-details";
+        } else {
+            String error = "Unable to find an account with that ID";
+            model.addAttribute("error", error);
+            return "errors/account-error";
+        }
+    }
 }
 
