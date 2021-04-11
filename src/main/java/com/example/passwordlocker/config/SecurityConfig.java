4adcd30d6eca5a6d126762a5043515b9c2cd658f
patrick <patrick@fedora.linux.local>
2021-04-11 13:06:35 -0500

    Added logging to logins

--- src/main/java/com/example/passwordlocker/config/SecurityConfig.java
+++ src/main/java/com/example/passwordlocker/config/SecurityConfig.java
@@ -1,5 +1,9 @@
 package com.example.passwordlocker.config;
 
+import com.example.passwordlocker.models.Log;
+import com.example.passwordlocker.models.User;
+import com.example.passwordlocker.repositories.LogRepository;
+import com.example.passwordlocker.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,11 +11,14 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -25,6 +32,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService appUserDetailsService;
 
+    @Autowired
+    private LogRepository logRepository;
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         // Whitelist the static content and main page
@@ -37,7 +49,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/", true)
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication auth) throws IOException, ServletException {
+                        User user = userRepository.getUserByUsername(auth.getName());
+                        logRepository.save(new Log(user.getUsername() + " logged in"));
+                        response.sendRedirect(request.getContextPath());
+                    }
+                })
                 // Leaving the failure handler in for debugging
                 .failureHandler(new AuthenticationFailureHandler() {
                     @Override
