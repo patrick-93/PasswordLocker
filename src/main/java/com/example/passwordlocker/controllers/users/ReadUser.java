c273fd57f51bbab882a01e8bc987531243826369
patrick <patrick@fedora.linux.local>
2021-04-11 19:06:39 -0500

    Added a user details page and modified the all users page to have a link to the user details

--- src/main/java/com/example/passwordlocker/controllers/users/ReadUser.java
+++ src/main/java/com/example/passwordlocker/controllers/users/ReadUser.java
@@ -1,11 +1,14 @@
 package com.example.passwordlocker.controllers.users;
 
+import com.example.passwordlocker.models.Account;
+import com.example.passwordlocker.models.User;
 import com.example.passwordlocker.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/users")
@@ -18,5 +21,16 @@ public class ReadUser {
     public String showAll(Model model) {
         model.addAttribute("users", userRepository.findAll());
         return "users/users";
+    }
+
+    @GetMapping("/details")
+    public String getUserDetails(@RequestParam(name="id") long id, Model model) {
+        User user = userRepository.findById(id).orElse(new User());
+        if (user.getUsername() == null) {
+            model.addAttribute("userNotFound", "Unable to find a user with ID: " + id);
+            return "users/user-details";
+        }
+        model.addAttribute("user", user);
+        return "users/user-details";
     }
 }
