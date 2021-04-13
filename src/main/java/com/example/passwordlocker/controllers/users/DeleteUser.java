d94f4e84d70aef771b21f5f30679e3fb032fa0a8
patrick <patrick@fedora.linux.local>
2021-04-12 19:50:08 -0500

    Implemented the DeleteUser controller, added role checking to DeleteUser and ReadUser, created unauthorized-access.html to show a generic error if the user doesn't have the correct role to view a page

--- src/main/java/com/example/passwordlocker/controllers/users/DeleteUser.java
+++ src/main/java/com/example/passwordlocker/controllers/users/DeleteUser.java
@@ -1,4 +1,46 @@
 package com.example.passwordlocker.controllers.users;
 
+import com.example.passwordlocker.models.Log;
+import com.example.passwordlocker.models.User;
+import com.example.passwordlocker.repositories.LogRepository;
+import com.example.passwordlocker.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class DeleteUser {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private LogRepository logRepository;
+
+    @GetMapping("/users/delete")
+    public String deleteUser(@RequestParam(name = "id", required = true) long id) {
+        User user = userRepository.findById(id).orElse(new User());
+
+        // Check if user valid, if not return to /users
+        if (user.getUsername() == null) {
+            return "redirect:/users";
+        }
+        // User is valid so get the current user
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = userRepository.getUserByUsername(auth.getName());
+
+        // Check if user has role of ADMIN
+        if (!currentUser.getRoles().equals("ADMIN")) {
+            return "errors/unauthorized-access";
+        }
+
+        // User has a role of ADMIN so delete and log
+        userRepository.deleteById(id);
+        logRepository.save(new Log(currentUser.getUsername() + " deleted " + user.getUsername()));
+
+        return "redirect:/users";
+
+    }
 }
--- src/main/java/com/example/passwordlocker/controllers/users/ReadUser.java
+++ src/main/java/com/example/passwordlocker/controllers/users/ReadUser.java
@@ -4,6 +4,8 @@ import com.example.passwordlocker.models.Account;
 import com.example.passwordlocker.models.User;
 import com.example.passwordlocker.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,18 +21,43 @@ public class ReadUser {
 
     @GetMapping("")
     public String showAll(Model model) {
-        model.addAttribute("users", userRepository.findAll());
-        return "users/users";
+        // Get current user
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = userRepository.getUserByUsername(auth.getName());
+
+        // Check if user has role of ADMIN or AUDITOR
+        if (currentUser.getRoles().equals("ADMIN") || currentUser.getRoles().equals("AUDITOR")) {
+            model.addAttribute("users", userRepository.findAll());
+            return "users/users";
+        }
+        // User does not have access so show error page
+        return "errors/unauthorized-access";
     }
 
     @GetMapping("/details")
     public String getUserDetails(@RequestParam(name="id") long id, Model model) {
-        User user = userRepository.findById(id).orElse(new User());
-        if (user.getUsername() == null) {
-            model.addAttribute("userNotFound", "Unable to find a user with ID: " + id);
+        // Get current user
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = userRepository.getUserByUsername(auth.getName());
+
+        // Check if user has role of ADMIN or AUDITOR
+        if (currentUser.getRoles().equals("ADMIN") || currentUser.getRoles().equals("AUDITOR")) {
+
+            // User is authorized so attempt to show details
+            User user = userRepository.findById(id).orElse(new User());
+            if (user.getUsername() == null) {
+
+                // User ID is not valid, show an error
+                model.addAttribute("userNotFound", "Unable to find a user with ID: " + id);
+                return "users/user-details";
+            }
+
+            // User ID is valid so show details
+            model.addAttribute("user", user);
             return "users/user-details";
         }
-        model.addAttribute("user", user);
-        return "users/user-details";
+        // Current user does not have access, show error page
+        return "errors/unauthorized-access";
+
     }
 }
