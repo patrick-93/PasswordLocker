e74b3be02bc575b02f752018265d3c482a91ebb8
patrick <patrick@fedora.linux.local>
2021-04-14 19:45:10 -0500

    Made the username field in edit-user readonly, created the postmapping in UpdateUser for normal users to be able to edit their own user object

--- src/main/java/com/example/passwordlocker/controllers/users/UpdateUser.java
+++ src/main/java/com/example/passwordlocker/controllers/users/UpdateUser.java
@@ -1,14 +1,19 @@
 package com.example.passwordlocker.controllers.users;
 
+import com.example.passwordlocker.models.Log;
 import com.example.passwordlocker.models.User;
 import com.example.passwordlocker.repositories.LogRepository;
 import com.example.passwordlocker.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -18,11 +23,13 @@ public class UpdateUser {
     private UserRepository userRepository;
     @Autowired
     private LogRepository logRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
 
     // Get mapping for the regular user edit, admin edit will be separate mapping
     @GetMapping("/users/edit")
-    public String editUser(@RequestParam(name = "id", required = true) long id, Model model) {
+    public String editUserGet(@RequestParam(name = "id", required = true) long id, Model model) {
         // find the user with the id param
         User user = userRepository.findById(id).orElse(new User());
 
@@ -43,5 +50,38 @@ public class UpdateUser {
         }
         // Current user is not the user to be edited
         return "errors/unauthorized-access";
+    }
+    // Post mapping for regular user edit
+    @PostMapping("/users/edit")
+    public String editUserPost(@ModelAttribute("user") User user, BindingResult bindingResult) {
+        // Get current logged in user
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = userRepository.getUserByUsername(auth.getName());
+
+        // Make sure currentUser is the same one trying to be modified
+        if (!currentUser.getUsername().equals(user.getUsername())) {
+            return "errors/unauthorized-access";
+        }
+
+        // currentUser and user are the same so get the original
+        User originalUser = userRepository.getUserByUsername(user.getUsername());
+
+        // Now check to make sure properties aren't null and modify
+        // the originalUser with fields from the edit-user form
+        if (!user.getFirstName().isEmpty()) {
+            originalUser.setFirstName(user.getFirstName());
+        }
+        if (!user.getLastName().isEmpty()) {
+            originalUser.setLastName(user.getLastName());
+        }
+        if (!user.getPassword().isEmpty()) {
+            originalUser.setPassword(passwordEncoder.encode(user.getPassword()));
+        }
+
+        // Now save the originalUser and log a message
+        userRepository.save(originalUser);
+        logRepository.save(new Log(currentUser.getUsername() + " modified " + originalUser.getUsername()));
+
+        return "redirect:/";
     }
 }
