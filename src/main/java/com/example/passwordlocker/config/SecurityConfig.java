21edf7e242bf6b7b86d205dc2853454ec1114856
patrick <patrick@fedora.linux.local>
2021-04-16 13:00:54 -0500

    Modified the h2 db config in application.properties, set spring security to ignore the h2 console url

--- src/main/java/com/example/passwordlocker/config/SecurityConfig.java
+++ src/main/java/com/example/passwordlocker/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
@@ -43,7 +44,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // Whitelist the static content and main page
         httpSecurity
                 .authorizeRequests()
-                .antMatchers("/", "/css/**", "/js/**", "/img/**", "/create-first-user")
+                .antMatchers(
+                        "/",
+                        "/css/**",
+                        "/js/**",
+                        "/img/**",
+                        "/create-first-user"
+                )
                 .permitAll()
                 .anyRequest()
                 .authenticated()
@@ -87,5 +94,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(appUserDetailsService);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+        web.ignoring().antMatchers("/h2-console/**");
     }
 }
